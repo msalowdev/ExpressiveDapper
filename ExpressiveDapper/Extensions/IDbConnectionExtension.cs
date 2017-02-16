aeb67686e39d7528e296214eded4537958eed5ee
Mark <Mark@DESKTOP-GNC0N0V>
2017-02-16 15:33:59 -0700

    Added an overload to update to pass in a transaction object.

--- ExpressiveDapper/Extensions/IDbConnectionExtension.cs
+++ ExpressiveDapper/Extensions/IDbConnectionExtension.cs
@@ -54,7 +54,13 @@ namespace ExpressiveDapper.Extensions
 
             con.Execute(statement.Statement, statement.Parameters);
         }
+        public static void Update<TTable>(this IDbConnection con, TTable objectToUpdate,
+         Expression<Func<TTable, bool>> where, Func<TTable, dynamic> fieldsToIgnore, IDbTransaction transaction) where TTable : ITable
+        {
+            SqlStatement statement = SqlGenerator.GenerateUpdateStatement<TTable>(objectToUpdate, where, fieldsToIgnore);
 
+            con.Execute(statement.Statement, statement.Parameters, transaction);
+        }
         public static void Update<TTable>(this IDbConnection con, dynamic objectToUpdate, Expression<Func<TTable, bool>> where, IDbTransaction transaction) where TTable : ITable
         {
             SqlStatement statement = SqlGenerator.GenerateUpdateStatement<TTable>(objectToUpdate, where);
